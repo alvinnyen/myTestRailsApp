074821e605fbe5202b19292dc9e3e535e3c8ff34
alvinnyen <alvinyen219@gmail.com>
2018-10-13 23:37:52 +0800

    [dataParser] complete getRestaurantList

--- app/javascript/dataParser.js
+++ app/javascript/dataParser.js
@@ -1,1 +1,28 @@
 const rawData = require('./data.json');
+
+function getRestaurantList (weekday) {
+    const data = [...rawData];
+
+    return data.map(({
+        id, 
+        name, 
+        photos: { 
+            0: backgroundImgUrl 
+        } = {},
+        location: {
+            address1: address
+        } = {},
+        hours: {
+            0: {
+                open = []
+            } = {}
+        } = {}
+    }) => ({
+        address,
+        backgroundImgUrl,
+        id,
+        name,
+        start: open[weekday] && open[weekday].start,
+        end: open[weekday] && open[weekday].end
+    }));
+}
