7bdab32e7ebcb8ba306ea6703a77167e91a4189f
alvinnyen <alvinyen219@gmail.com>
2018-10-14 15:37:45 +0800

    [dataParser] complete getTheRestaurantDetail

--- app/javascript/dataParser.js
+++ app/javascript/dataParser.js
@@ -41,3 +41,45 @@ function _isOpen (start, end) {
 
     return moment().isBetween(startTime , endTime)
 }
+
+module.exports.getTheRestaurantDetail = function (restaurantId, weekday) {
+    const data = [...rawData];
+    const {
+        id,
+        name,
+        rating,
+        review_count: reviewCount,
+        price,
+        categories = [],
+        photos: { 
+            0: backgroundImgUrl 
+        } = {},
+        location: {
+            address1: address
+        } = {},
+        hours: {
+            0: {
+                open = []
+            } = {}
+        } = {}
+    } = data[restaurantId];
+
+    const start = open[weekday] && open[weekday].start;
+    const end = open[weekday] && open[weekday].end;
+    
+    return {
+        id,
+        name,
+        address,
+        rating,
+        price,
+        categories: categories.map(({title}) => title).join(', '),
+        reviewCount,
+        backgroundImgUrl,
+        start,
+        end,
+        isOpen: _isOpen(start, end)
+    };
+};
+
+console.log(module.exports.getTheRestaurantDetail(1, 1));
