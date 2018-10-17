9b710e51d4f97fb8fc72b1cf1dff273354495651
alvinnyen <alvinyen219@gmail.com>
2018-10-17 18:09:35 +0800

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  match '*path', to: 'pages#index', via: :all # it will catch all, then sent it to pages#action
+  match '*path', to: 'pages#index', :via => :all # it will catch all, then sent it to pages#action
 end
